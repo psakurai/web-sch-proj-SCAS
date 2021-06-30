d08839b48267b4d08575795d484910c9bdac8417
arif amiruddin <76756035+psakurai@users.noreply.github.com>
2021-06-30 16:58:34 +0800

    Update config.php

--- Function/Init/config.php
+++ Function/Init/config.php
@@ -5,5 +5,5 @@
     $db_name='mydatabase';
 
     $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("Unable to connect");
-    echo"Test 1";
+    echo"Connected Successfully";
 ?>
