b2a0f93e536438b7d89a0ee160ca3c8f4504b386
Afiq Nazrie <57625126+mangkoran@users.noreply.github.com>
2021-06-30 21:29:57 +0800

    Fix config.php path

--- Function/Init/createUser.php
+++ Function/Init/createUser.php
@@ -1,6 +1,7 @@
 <?php
-  require ("config.php");
-  $sql = "CREATE TABLE User (
+require "../Init/config.php";
+
+$sql = "CREATE TABLE User (
     Username VARCHAR(255) NOT NULL,
     Password VARCHAR(255) NOT NULL,
     IdentificationID VARCHAR(255) NOT NULL,
@@ -11,14 +12,13 @@
     Email VARCHAR(255) NOT NULL,
     Address VARCHAR(255) NOT NULL,
     PRIMARY KEY (IdentificationID)
-  )";
+    )";
 
-  if (mysqli_query($conn, $sql)) {
+if (mysqli_query($conn, $sql)) {
     echo "<h3>Table User created successfully</h3>";
-  }
-  else {
+} else {
     echo "Error creating table: " . mysqli_error($conn);
-  }
+}
 
-  mysqli_close($conn);
+mysqli_close($conn);
 ?>
