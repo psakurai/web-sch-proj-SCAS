a4cac0b5b6ec81f200846520c24cc6fabc03a445
Afiq Nazrie <57625126+mangkoran@users.noreply.github.com>
2021-06-30 15:06:45 +0800

    Change db password

--- Function/Init/config.php
+++ Function/Init/config.php
@@ -1,7 +1,7 @@
  <?php
     $db_host='localhost';
     $db_user='root';
-    $db_pass='';
+    $db_pass='password';
     $db_name='mydatabase';
 
     $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
