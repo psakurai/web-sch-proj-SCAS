126516e6251fb1c0d86a336fd3696f58e00df126
Afiq Nazrie <57625126+mangkoran@users.noreply.github.com>
2021-06-30 23:06:35 +0800

    Change Edit User implementation
    If Admin did not enter value in one of the input field, the value will remain the same

--- Function/Admin/editUser.php
+++ Function/Admin/editUser.php
@@ -12,20 +12,21 @@ $phone = $_POST["phone"];
 $email = $_POST["email"];
 $address = $_POST["address"];
 
-// print "$username $password $id $userlevel $firstname $lastname $phone $email $address";
 $sql = "UPDATE User
     SET
-    Username = '$username',
-    Password = '$password',
-    User_Level = '$userlevel',
-    First_Name = '$firstname',
-    Last_Name = '$lastname',
-    Phone_No = '$phone',
-    Email = '$email',
-    Address = '$address'
+    Username = CASE WHEN '$username' = '' THEN Username ELSE '$username' END,
+    Password = CASE WHEN '$password' = '' THEN Password ELSE '$password' END,
+    User_Level = CASE WHEN '$userlevel' = '' THEN User_Level ELSE '$userlevel' END,
+    First_Name = CASE WHEN '$firstname' = '' THEN First_Name ELSE '$firstname' END,
+    Last_Name = CASE WHEN '$lastname' = '' THEN Last_Name ELSE '$lastname' END,
+    Phone_No = CASE WHEN '$phone' = '' THEN Phone_No ELSE '$phone' END,
+    Email = CASE WHEN '$email' = '' THEN Email ELSE '$email' END,
+    Address = CASE WHEN '$address' = '' THEN Address ELSE '$address' END
     WHERE IdentificationID = '$id'";
+$columncheck = "SELECT EXISTS (SELECT * FROM User WHERE IdentificationID = '$id')";
 
 if (mysqli_query($conn, $sql)) {
+    // mysqli_query($conn, $sql);
     echo "<h3>User edited successfully</h3>";
 } else {
     echo "Error editing user: " . mysqli_error($conn);
