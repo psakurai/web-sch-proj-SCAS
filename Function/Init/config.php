2ad4c4a4d0a5b6011fff03af811ba7e70998b622
Afiq Nazrie <57625126+mangkoran@users.noreply.github.com>
2021-07-02 00:03:45 +0800

    Remove conn output
    conn if else and output is removed in order to make mainManager php query to work properly.

--- Function/Init/config.php
+++ Function/Init/config.php
@@ -4,6 +4,7 @@
     $db_pass='password';
     $db_name='mydatabase';
 
-    $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("Unable to connect");
-    echo"Connected Successfully";
+    // $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name) or die("Unable to connect");
+    // echo"Connected Successfully";
+    $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 ?>
