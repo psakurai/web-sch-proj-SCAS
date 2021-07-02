ccec4732aa6eb9382bb969652bb25e867c0410d1
Afiq Nazrie <57625126+mangkoran@users.noreply.github.com>
2021-07-02 11:33:33 +0800

    Invoke updateAcceptedRequestCount

--- Function/Manager/getAnalytic.js
+++ Function/Manager/getAnalytic.js
@@ -2,16 +2,19 @@ let xhttp;
 let reqcnt = '../../Function/Manager/getRequestCount.php';
 let accreqcnt = '../../Function/Manager/getAccetedRequestCount.php';
 
-window.onload = setInterval(function () { makeRequest(reqcnt); }, 1000);
+window.onload = setInterval(function () {
+    makeRequest(reqcnt, updateRequestCount);
+    makeRequest(accreqcnt, updateAcceptedRequestCount);
+}, 1000);
 
-function makeRequest(url) {
+function makeRequest(url, update) {
     xhttp = new XMLHttpRequest();
 
     if (!xhttp) {
         alert('Giving up :( Cannot create an XMLHTTP instance ');
         return false;
     }
-    xhttp.onreadystatechange = updateRequestCount;
+    xhttp.onreadystatechange = update;
     xhttp.open('GET', url);
     xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhttp.send();
