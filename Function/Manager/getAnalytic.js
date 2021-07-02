f7c354a7f61bac6bcec8fb8a5ffd2acb8574ab8c
Afiq Nazrie <57625126+mangkoran@users.noreply.github.com>
2021-07-02 20:50:45 +0800

    Code refactor

--- Function/Manager/getAnalytic.js
+++ Function/Manager/getAnalytic.js
@@ -1,16 +1,32 @@
-let reqcnturl = "../../Function/Manager/getRequestCount.php";
-let accreqcnturl = '../../Function/Manager/getAcceptedRequestCount.php';
-let rejreqcnturl = "../../Function/Manager/getRejectedRequestCount.php";
-
 window.onload = setInterval(() => {
+    const reqcnturl = '../../Function/Manager/getRequestCount.php';
+    const accreqcnturl = '../../Function/Manager/getAcceptedRequestCount.php';
+    let rejreqcnturl = "../../Function/Manager/getRejectedRequestCount.php";
     const reqcnt = makeRequest(reqcnturl, updateRequestCount);
     const accreqcnt = makeRequest(accreqcnturl, updateAcceptedRequestCount);
     const rejreqcnt = makeRequest(rejreqcnturl, updateRejectedRequestCount);
 }, 1000);
 
+const updateRequestCount = (response) => {
+    console.log(response);
+    // const response = JSON.parse(responseText);
+    document.getElementById("request").value = response.resultCnt;
+}
+
+const updateAcceptedRequestCount = (response) => {
+    console.log(response);
+    // const response = JSON.parse(responseText);
+    document.getElementById("accepted-request").value = response.accResultCnt;
+}
+
+const updateRejectedRequestCount = (responseText) => {
+    console.log(responseText);
+    const response = JSON.parse(responseText);
+    document.getElementById("rejected-request").value = response.rejResultCnt;
+}
+
 const makeRequest = (url, update) => {
-    let xhttp;
-    xhttp = new XMLHttpRequest();
+    const xhttp = new XMLHttpRequest();
 
     if (!xhttp) {
         alert('Giving up :( Cannot create an XMLHTTP instance ');
@@ -20,30 +36,13 @@ const makeRequest = (url, update) => {
     xhttp.onreadystatechange = () => {
         if (xhttp.readyState === XMLHttpRequest.DONE) {
             if (xhttp.status === 200) {
-                console.log(xhttp.responseText);
-                update(xhttp.responseText);
+                // console.log(xhttp.responseText);
+                const response = JSON.parse(xhttp.responseText);
+                update(response);
             }
         }
     };
     xhttp.open('POST', url);
     xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
     xhttp.send();
-}
-
-const updateRequestCount = (responseText) => {
-    console.log(responseText);
-    const response = JSON.parse(responseText);
-    document.getElementById("request").value = response.resultCnt;
-}
-
-const updateAcceptedRequestCount = (responseText) => {
-    console.log(responseText);
-    const response = JSON.parse(responseText);
-    document.getElementById("accepted-request").value = response.accResultCnt;
-}
-
-const updateRejectedRequestCount = (responseText) => {
-    console.log(responseText);
-    const response = JSON.parse(responseText);
-    document.getElementById("rejected-request").value = response.rejResultCnt;
 }
