4d556f6238d85e6229d61352e3f50ea1ac2926fa
Afiq Nazrie <57625126+mangkoran@users.noreply.github.com>
2021-07-09 21:37:36 +0800

    Change input form layout to table

--- User/Student/applyCollege.php
+++ User/Student/applyCollege.php
@@ -47,10 +47,50 @@ session_start();
                     <h2>Apply College</h2>
                     <form id="apply-college-form" method="POST" name="apply-college-form" onsubmit="return applyCollege()">
                         <div class="input-data">
-                            <label for="college-name">College Name</label>
+                            <!-- <label for="college-name">College Name</label>
                             <input type="text" name="college-name" />
                             <label for="building-no">Building No</label>
-                            <input type="text" name="building-no" />
+                            <input type="text" name="building-no" /> -->
+                            <table>
+                                <tr>
+                                    <td><label for="college-name">College Name</label></td>
+                                    <td>
+                                        <select id="college-name" name="college-name">
+                                            <?php
+                                                require_once('../../Function/Init/config.php');
+
+                                                $sqlcollege = "SELECT * FROM College";
+                                                $sqlresult = mysqli_query($conn, $sqlcollege);
+
+                                                if (mysqli_num_rows($sqlresult) > 0) {
+                                                    while ($row = mysqli_fetch_assoc($sqlresult)) {
+                                                        echo '<option value="', $row["College_Name"], '">', $row["College_Name"], '</option>';
+                                                    }
+                                                }
+                                            ?>
+                                        </select>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td><label for="building-no">Building No</label></td>
+                                    <td>
+                                        <select id="college-name" name="college-name">
+                                            <?php
+                                                require_once('../../Function/Init/config.php');
+
+                                                $sqlcollege = "SELECT * FROM College";
+                                                $sqlresult = mysqli_query($conn, $sqlcollege);
+
+                                                if (mysqli_num_rows($sqlresult) > 0) {
+                                                    while ($row = mysqli_fetch_assoc($sqlresult)) {
+                                                        echo '<option value="', $row["Building_No"], '">', $row["Building_No"], '</option>';
+                                                    }
+                                                }
+                                            ?>
+                                        </select>
+                                    </td>
+                                </tr>
+                            </table>
                         </div>
                         <input type="submit" name="submit" value="Apply College">
                     </form>
