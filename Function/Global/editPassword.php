1ea1a72b91153bd9e82919514d787f6fd463e8d6
madsaintis <83106614+madsaintis@users.noreply.github.com>
2021-07-09 19:24:14 +0800

    Update editPassword.php
    change ID to username

--- Function/Global/editPassword.php
+++ Function/Global/editPassword.php
@@ -52,9 +52,9 @@ echo "<link rel='stylesheet' type='text/css' href='../../Assets/css/style.css' /
 
 <form action ="updateInformation.php" method="post">
 <?php
-  $currentUser = $_SESSION["ID"];
+  $currentUser = $_SESSION["USER"];
 
-  $sql = "SELECT * FROM User WHERE IdentificationID = '$currentUser'";
+  $sql = "SELECT * FROM User WHERE Username = '$currentUser'";
   $result = mysqli_query($conn,$sql);
 
   if($result)
